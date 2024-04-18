b6fd2907a17713adde5344a695f904488691096c
João Neto <jflorencioadv@gmail.com>
2024-04-18 11:09:57 +0200

    joao

--- src/components/MyFormChallenge.jsx
+++ src/components/MyFormChallenge.jsx
@@ -1,10 +1,27 @@
-
+import { useRef, useState} from 'react';
+import BoxColor from "./BoxColor";
 
 function MyForm() {
+  const [value, setValue] = useState('');
+  const inputRef = useRef('');
+  const colors = ['red', 'green', 'pink', 'yellow', 'purple', 'white', 'blue', 'aqua', 'olive'];
 
   return (
-    <></>
-  );
-}
+    <>
+      <input 
+        type='text'
+        placeholder='choose a color'
+        value={value}
+        ref={inputRef}
+        onChange={() => setValue(inputRef.current.value)}
+      />
+      <div className="container">
+        {colors.map((color, index) => (
+            <BoxColor key={index} color={color} value={value} />
+        ))}
+      </div>
 
+    </>
+  )
+}
 export default MyForm;
